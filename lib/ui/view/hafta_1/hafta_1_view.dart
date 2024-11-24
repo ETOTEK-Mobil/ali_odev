9313c191317376ab21151919dd1c22a8d377a347
Berke Enes Aktümen <81863556+berkeenesakt@users.noreply.github.com>
2024-11-24 09:34:55 +0300

    Revert "hafta 1 i ekledim :)"

--- lib/ui/view/hafta_1/hafta_1_view.dart
+++ lib/ui/view/hafta_1/hafta_1_view.dart
@@ -1,45 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:odev/ui/view/hafta_1/pages/column_row_stack_page.dart';
-import 'package:odev/ui/view/hafta_1/pages/state_managment_page.dart';
 
 class Hafta1View extends StatelessWidget {
   const Hafta1View({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 233, 211, 211),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: 200,
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                ),
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) =>const Page1()));
-                },
-                child:const Text("Columb, Row and Stack"),
-                ),
-            ),
-            SizedBox(
-              width: 200,
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                ),
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) =>const Page2()));
-                },
-                child:const Text("State Managment"),
-                ),
-            ),
-          ],
-        ),
-      )
-    );
+    return const Placeholder();
   }
 }
