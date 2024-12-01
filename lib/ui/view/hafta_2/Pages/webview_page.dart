7ecfe67ee8645d0e3acdd7eb351bfe70d77ec633
Berke Enes Aktümen <81863556+berkeenesakt@users.noreply.github.com>
2024-12-01 07:25:38 +0300

    Refactor WebviewPage: Improve code readability and restore super.initState() call; remove unused widget test file

--- lib/ui/view/hafta_2/Pages/webview_page.dart
+++ lib/ui/view/hafta_2/Pages/webview_page.dart
@@ -9,44 +9,42 @@ class WebviewPage extends StatefulWidget {
 }
 
 class _WebviewPageState extends State<WebviewPage> {
-
   late WebViewController controller;
 
   @override
   void initState() {
-    //super.initState();
     controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..loadRequest(Uri.parse("https://www.google.com"));
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse("https://www.google.com"));
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text("WebView"),
+        title: const Text("WebView"),
         actions: [
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: IconButton(
-              onPressed: (){
-                setState(() {
-                  initState();
-                });
-              }, 
-              icon:const Icon(Icons.refresh)),
+                onPressed: () {
+                  setState(() {
+                    initState();
+                  });
+                },
+                icon: const Icon(Icons.refresh)),
           ),
           IconButton(
-            onPressed: (){
-              setState(() {
-                controller = WebViewController()
-                ..setJavaScriptMode(JavaScriptMode.unrestricted)
-                ..loadRequest(Uri.parse("https://www.etotek.com.tr"));
-                WebViewWidget(controller: controller);
-              });
-            }, 
-            icon: Image.asset("lib/images/etotek.ico")
-            )
+              onPressed: () {
+                setState(() {
+                  controller = WebViewController()
+                    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+                    ..loadRequest(Uri.parse("https://www.etotek.com.tr"));
+                  WebViewWidget(controller: controller);
+                });
+              },
+              icon: Image.asset("lib/images/etotek.ico"))
         ],
       ),
       body: WebViewWidget(controller: controller),
--- lib/ui/view/hafta_2/hafta_2_view.dart
+++ lib/ui/view/hafta_2/hafta_2_view.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:odev/core/constants/app_sizes.dart';
-import 'package:odev/ui/view/hafta_2/Pages/buttons_page.dart';
-import 'package:odev/ui/view/hafta_2/Pages/images_page.dart';
-import 'package:odev/ui/view/hafta_2/Pages/picker_page.dart';
-import 'package:odev/ui/view/hafta_2/Pages/progress_slider_bar_page.dart';
-import 'package:odev/ui/view/hafta_2/Pages/text_text_filed_page.dart';
-import 'package:odev/ui/view/hafta_2/Pages/webview_page.dart';
+import 'package:odev/ui/view/hafta_2/pages/buttons_page.dart';
+import 'package:odev/ui/view/hafta_2/pages/images_page.dart';
+import 'package:odev/ui/view/hafta_2/pages/picker_page.dart';
+import 'package:odev/ui/view/hafta_2/pages/progress_slider_bar_page.dart';
+import 'package:odev/ui/view/hafta_2/pages/text_text_filed_page.dart';
+import 'package:odev/ui/view/hafta_2/pages/webview_page.dart';
 import 'package:odev/ui/widgets/buttons/common_button.dart';
 
 class Hafta2View extends StatelessWidget {
@@ -20,13 +20,13 @@ class Hafta2View extends StatelessWidget {
     "Dropdown Button & Time Date Picker"
   ];
 
-  final  _pages =const [
-  TextTextFieldPage(),
-  ButtonsPage(),
-  ProgressSliderBarPage(),
-  WebviewPage(),
-  ImagesPage(),
-  PickerPage(),
+  final _pages = const [
+    TextTextFieldPage(),
+    ButtonsPage(),
+    ProgressSliderBarPage(),
+    WebviewPage(),
+    ImagesPage(),
+    PickerPage(),
   ];
 
   @override
@@ -34,16 +34,15 @@ class Hafta2View extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(30.0),
       child: ListView.separated(
-        itemBuilder: (context, index){
-          return CommonButton(page: _pages[index], pageName: _pagaNames[index]);
-        }, 
-        separatorBuilder: (context, index){
-          return const SizedBox(
-            height: AppSize.md,
-          );
-        }, 
-        itemCount: _pagaNames.length
-        ),
+          itemBuilder: (context, index) {
+            return CommonButton(page: _pages[index], pageName: _pagaNames[index]);
+          },
+          separatorBuilder: (context, index) {
+            return const SizedBox(
+              height: AppSize.md,
+            );
+          },
+          itemCount: _pagaNames.length),
     );
   }
 }
